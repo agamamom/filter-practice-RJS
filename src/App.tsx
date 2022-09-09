b92618a728bb7f468bc37a5953866aa7b17f2e90
dathoang <dathoang31082001@gmail.com>
2022-09-09 20:58:53 +0700

    update typescript

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { useState, useEffect, ChangeEvent } from 'react';
+import CardList from './components/card-list/card-list.jsx';
+import SearchBox from './components/search-box/search-box.js';
+import './App.css'
+import { getData } from './utils/data.utils.js';
+
+export type Monster = {
+  id: string,
+  name: string,
+  email: string,
+}
+
+const App = () => {
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState<Monster[]>([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
+
+  useEffect(() => {
+
+    const fetchUser = async () => {
+      const users = await getData<Monster[]>('https://jsonplaceholder.typicode.com/users')
+      setMonsters(users);
+    };
+
+    fetchUser();
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  return (
+    <div className='App'>
+      <h1 className='app-title'>Monsters Rolodex</h1>
+      <SearchBox
+        className='monsters-search-box'
+        onChangeHandler={onSearchChange}
+        placeholder='search monsters'
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
+
+export default App;
